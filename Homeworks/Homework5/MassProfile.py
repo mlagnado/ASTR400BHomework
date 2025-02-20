7f8951d9180f00dc3707fa0b1877c246b0a9df86
mlagnado <mlagnado@arizona.edu>
2025-02-20 13:24:02 -0700

    Commented through the code

--- Homeworks/Homework5/MassProfile.py
+++ Homeworks/Homework5/MassProfile.py
@@ -9,85 +9,137 @@ from astropy import constants as const
 class MassProfile:
 
     def __init__(self,galaxy,snap):
-        snap_num = '000'+str(snap)
-        snap_num = snap_num[-3:]
-        self.filename = "%s_"%(galaxy) + snap_num + '.txt'
+        '''
+        This initializes the class getting the data from the file and creating the function name
+        Inputs: 
+            galaxy (string) should be the name of the galaxy ex. 'MW' for milky way
+            snap (int) the snap number of the desired data
+                       This i srelated to the timestep of the simulation.
+        '''
+        snap_num = '000'+str(snap) #Turns the snap number into a string
+        snap_num = snap_num[-3:] #Makes sure the snap number is only 3 characters long
+        self.filename = "%s_"%(galaxy) + snap_num + '.txt' #Connects the galaxy name and snap number to get the desired filename
 
-        self.time, self.total, self.data = Read(self.filename)
-        self.x = self.data['x']*u.kpc
+        self.time, self.total, self.data = Read(self.filename) #Reads in data from the file
+        self.x = self.data['x']*u.kpc #initializes x position data with units
         self.y = self.data['y']*u.kpc
         self.z = self.data['z']*u.kpc
         self.m = self.data['m']
-        self.gname = galaxy
+        self.gname = galaxy #collects the galaxy name for later use
 
 
     def MassEnclosed(self,ptype,radii):
-
-        COM = CenterOfMass(self.filename,ptype)
-
-        COM_p = COM.COM_P(0.1)
-        indexP = np.where(self.data['type'] == ptype)
-        xR = self.x-COM_p[0]
+        '''
+        This function Calculates the mass enclosed by a desired radius and particle type
+        Inputs:
+            ptype (int) this relates to the particle type 1-halo, 2-disk, 3-bulge
+            radii (list of floats) These are the radii where the mass enclosed will be calculated at.
+        Outputs:
+            A list of masses from each radius in units of solar masses
+        '''
+        COM = CenterOfMass(self.filename,ptype) #Calculates the center of mass
+        COM_p = COM.COM_P(0.1) #Calculates the position coordinates of the COM
+        indexP = np.where(self.data['type'] == ptype) #Finds the data of a specific particle type
+        xR = self.x-COM_p[0] #Shifts the x position data by the COM coords
         yR = self.y-COM_p[1]
         zR = self.z-COM_p[2]
-        xP = xR[indexP]
+        xP = xR[indexP] #Filters the x position data by the particle type
         yP = yR[indexP]
         zP = zR[indexP]
         mP = self.m[indexP]
-        R = (xP**2 + yP**2 + zP**2)**0.5
-        Masses = np.zeros(shape=len(radii))
+        R = (xP**2 + yP**2 + zP**2)**0.5 #Calculates the radius at each point
+        Masses = np.zeros(shape=len(radii)) #Initializes a list of masses (Same length as list of radii)
         for i in range(len(radii)):
-            indexR = np.where(R < radii[i]*u.kpc)
-            m_tot = np.sum(mP[indexR])
-            Masses[i] = m_tot
-        Masses = Masses*1e10
-        return Masses*u.Msun
+            indexR = np.where(R < radii[i]*u.kpc) #Finds points where the radius is less than the desired enclosed radius
+            m_tot = np.sum(mP[indexR]) #Sums the mass inside the enclosed radius
+            Masses[i] = m_tot #Adds the sum to the list
+        Masses = Masses*1e10 #Adjusts the values for units
+        return Masses*u.Msun #returns and adds units to the output
 
     def MassEnclosedTotal(self,radii):
-        MHalo = self.MassEnclosed(1,radii)
-        MDisk = self.MassEnclosed(2,radii)
-        if self.gname == 'M33':
+        '''
+        This function sums the mass of all particle types 
+        Input:
+            radii (list of floats) These are the radii where the mass enclosed will be calculated at
+        Output:
+            MTotal (list of astropy quantities) These are the total enclosed mass (units of Msun) at each input radius
+        '''
+        MHalo = self.MassEnclosed(1,radii) #calculates the mass enclosed by halo particle type
+        MDisk = self.MassEnclosed(2,radii) #calculates the mass enclosed by disk particle type
+        if self.gname == 'M33': #M33 does not have a bulge particle which we have to account for
             MTotal = MHalo + MDisk
-        else:
-            MBulge = self.MassEnclosed(3,radii)
+        else: #If not M33 we calculate bulge enclosed mass
+            MBulge = self.MassEnclosed(3,radii) #Calculates the mass enclosed by bulge particle type
             MTotal = MHalo + MDisk + MBulge
         return MTotal
 
     def HernquistMass(self,radii,a,Mhalo):
-        numerator = Mhalo*(radii**2)
-        denominator = (a+radii)**2
-        halo_mass = numerator/denominator
+        '''
+        This functions calculates the Hernquist Mass at each radius using the halo mass
+        Inputs:
+            radii (list of floats) These are the radii where the mass enclosed was calculated
+            a (float) The hernquist scale radius 
+            Mhalo (list of astropy quantities) The enclosed halo mass at each radius
+        Output:
+            halo_mass (list of floats) The calculated hernquist mass at each input radius
+        '''
+        numerator = Mhalo*(radii**2) #Calculates the numerator of the hernquist profile equation
+        denominator = (a+radii)**2 #Calculates the denominator of the hernquist profile equation
+        halo_mass = numerator/denominator #Calculkates the hernquist mass profile
         return halo_mass
 
     def CircularVelocity(self,ptype,radii):
-        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun)
-        Mass = self.MassEnclosed(ptype,radii)
-        circvel = np.sqrt(const.G*Mass/radii)
+        '''
+        This function calculates the circular velocity of each particle type at each radius
+        Inputs:
+            ptype (int) the desired particle type 1-halo,2-disk,3-bulge
+            radii (list of floats) The radii at which the circular velocity of the mass enclosed will be calculated
+        '''
+        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun) #conversion of the G constant to desired units
+        Mass = self.MassEnclosed(ptype,radii) #Calculates the mass enclosed at each radius
+        circvel = np.sqrt(const.G*Mass/radii) #Calculates the circular velocity of the enclosed mass at each radius
         return circvel
     
     def CircularVelocityTotal(self,radii):
-        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun)
-        MTotal = self.MassEnclosedTotal(radii)
-        VTotal = np.sqrt(const.G*MTotal/radii)
+        '''
+        This function calculates the total circular velocity of all particle types at each radius
+        Input:
+            radii (list of floats) The radius at which the circular velocity of all particle types will be calculated at
+        Output:
+            VTotal (list of floats) The calculated total circular velocity at each input radius
+        '''
+        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun) #Converts G constant to the desired units
+        MTotal = self.MassEnclosedTotal(radii) #Calculates the total enclosed mass at each radius
+        VTotal = np.sqrt(const.G*MTotal/radii) #Calculates the total circular velocity at each enclosed radius
         return VTotal
 
-    def HernquistVCirc(self,radius,a,Mhalo):
-        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun)
-        Mass = self.HernquistMass(radius,a,Mhalo)
-        VCirc = np.sqrt(const.G*Mass/radius)
+    def HernquistVCirc(self,radii,a,Mhalo):
+        '''
+        This function calculates the hernquist circular velocity
+        Inputs:
+            radius (list of floats) The radii at which the hernquist circular velocity will be calculated at
+            a (float) The hernquist scale radius
+            Mhalo (list of astropy quantities) The enclosed mass of the halo at each radius
+        '''
+        const.G = const.G.to(u.kpc*u.km**2/u.s**2/u.Msun) #Converts the G constant into desired units
+        Mass = self.HernquistMass(radii,a,Mhalo) #Calculates the hernquist mass at each radius
+        VCirc = np.sqrt(const.G*Mass/radii) #Calculates the circular velocity of the hernquist mass at each radius
         return VCirc
 
 
 
 if __name__ == '__main__' :
-    MW = MassProfile("MW", 0)
-    rs = np.arange(0.1,30.0,0.5)
-    MWmassHalo = MW.MassEnclosed(1,rs)
-    MWmassDisk = MW.MassEnclosed(2,rs)
-    MWmassBulge = MW.MassEnclosed(3,rs)
-    MWmassTotal = MW.MassEnclosedTotal(rs)
+    '''
+    Plots all the plots for the homework
+    '''
+    MW = MassProfile("MW", 0) #creates the mass profile for MW
+    rs = np.arange(0.1,30.0,0.5) #List of radii used for all plots
+    MWmassHalo = MW.MassEnclosed(1,rs) #Calculates the enclosed halo mass at each radius
+    MWmassDisk = MW.MassEnclosed(2,rs) #Calculates the enclosed disk mass at each radius
+    MWmassBulge = MW.MassEnclosed(3,rs) #Calculates the enclosed bulge mass at each radius
+    MWmassTotal = MW.MassEnclosedTotal(rs) #Calculates the enclosed total mass at each radius
     a = 60 #Used in class for MW
-    MWHernquist = MW.HernquistMass(rs,a,MWmassHalo)
+    MWHernquist = MW.HernquistMass(rs,a,MWmassHalo) #Calculates the hernquist mass at each radius
     fig = plt.figure(figsize=(7,4))
     ax = plt.subplot(111)
     plt.plot(rs,MWmassHalo, label='Halo Mass Profile')
@@ -105,12 +157,13 @@ if __name__ == '__main__' :
     plt.show()
     plt.savefig('MWMass.png')
     
-    M31 = MassProfile("M31", 0)
-    M31massHalo = M31.MassEnclosed(1,rs)
-    M31massDisk = M31.MassEnclosed(2,rs)
-    M31massBulge = M31.MassEnclosed(3,rs)
-    M31massTotal = M31.MassEnclosedTotal(rs)
-    M31Hernquist = M31.HernquistMass(rs,a,M31massHalo)
+    M31 = MassProfile("M31", 0) #Creates the mass profile for M31
+    M31massHalo = M31.MassEnclosed(1,rs) #Calculates the enclosed halo mass at each radius
+    M31massDisk = M31.MassEnclosed(2,rs)#Calculates the enclosed disk mass at each radius
+    M31massBulge = M31.MassEnclosed(3,rs)#Calculates the enclosed bulge mass at each radius
+    M31massTotal = M31.MassEnclosedTotal(rs) #Calculates the enclosed total mass at each radius
+    a = 60
+    M31Hernquist = M31.HernquistMass(rs,a,M31massHalo) #Calculates the hernquist mass at each radius
     ax = plt.subplot(111)
     plt.plot(rs,M31massHalo, label='Halo Mass Profile')
     plt.plot(rs,M31massDisk, label='Disk Mass Profile')
@@ -127,12 +180,13 @@ if __name__ == '__main__' :
     plt.show()
     plt.savefig('M31Mass.png')
 
-    M33 = MassProfile("M33", 0)
-    M33massHalo = M33.MassEnclosed(1,rs)
-    M33massDisk = M33.MassEnclosed(2,rs)
+    M33 = MassProfile("M33", 0) #Creates the mas profile for M33
+    M33massHalo = M33.MassEnclosed(1,rs) #Calculates the enclosed halo mass at each radius
+    M33massDisk = M33.MassEnclosed(2,rs) #Calculates the enclosed disk mass at each radius
     #M33 does not have a Bulge
-    M33massTotal = M33.MassEnclosedTotal(rs)
-    M33Hernquist = M33.HernquistMass(rs,a,M33massHalo)
+    M33massTotal = M33.MassEnclosedTotal(rs) #Calculates the enclosed total mass at each radius
+    a = 30
+    M33Hernquist = M33.HernquistMass(rs,a,M33massHalo) #Calculates the hernquist mass at each radius
     ax = plt.subplot(111)
     plt.plot(rs,M33massHalo, label='Halo Mass Profile')
     plt.plot(rs,M33massDisk, label='Disk Mass Profile')
@@ -149,11 +203,12 @@ if __name__ == '__main__' :
     plt.savefig('M33Mass.png')
 
     #plot 2
-    MWRotHalo = MW.CircularVelocity(1,rs)
-    MWRotDisk = MW.CircularVelocity(2,rs)
-    MWRotBulge = MW.CircularVelocity(3,rs)
-    MWRotCurve=MW.CircularVelocityTotal(rs)
-    MWHernquistCirc = MW.HernquistVCirc(rs,a,MWmassHalo)
+    a = 60 #Used in class for MW
+    MWRotHalo = MW.CircularVelocity(1,rs) #Calculates the circular velocity for the enclosed halo mass
+    MWRotDisk = MW.CircularVelocity(2,rs) #Calculates the circular velocity for the enclosed disk mass
+    MWRotBulge = MW.CircularVelocity(3,rs) #Calculates the circular velocity for the enclosed bulge mass
+    MWRotCurve=MW.CircularVelocityTotal(rs)  #Calculates the circular velocity for the enclosed total mass
+    MWHernquistCirc = MW.HernquistVCirc(rs,a,MWmassHalo)  #Calculates the circular velocity for the hernquist mass
     ax = plt.subplot(111)
     plt.plot(rs,MWRotHalo, label='Halo Rotation Curve')
     plt.plot(rs,MWRotDisk, label='Disk Rotation Curve')
@@ -167,11 +222,12 @@ if __name__ == '__main__' :
     plt.show()
     plt.savefig('MWVel.png')
 
-    M31RotHalo = M31.CircularVelocity(1,rs)
-    M31RotDisk = M31.CircularVelocity(2,rs)
-    M31RotBulge = M31.CircularVelocity(3,rs)
-    M31RotCurve=M31.CircularVelocityTotal(rs)
-    M31HernquistCirc = M31.HernquistVCirc(rs,a,M31massHalo)
+    a = 60
+    M31RotHalo = M31.CircularVelocity(1,rs) #Calculates the circular velocity for the enclosed halo mass
+    M31RotDisk = M31.CircularVelocity(2,rs) #Calculates the circular velocity for the enclosed disk mass
+    M31RotBulge = M31.CircularVelocity(3,rs) #Calculates the circular velocity for the enclosed bulge mass
+    M31RotCurve=M31.CircularVelocityTotal(rs) #Calculates the circular velocity for the enclosed total mass
+    M31HernquistCirc = M31.HernquistVCirc(rs,a,M31massHalo) #Calculates the circular velocity for the hernquist mass
     ax = plt.subplot(111)
     plt.plot(rs,M31RotHalo, label='Halo Rotation Curve')
     plt.plot(rs,M31RotDisk, label='Disk Rotation Curve')
@@ -185,11 +241,12 @@ if __name__ == '__main__' :
     plt.show()
     plt.savefig('M31Vel.png')
 
-    M33RotHalo = M33.CircularVelocity(1,rs)
-    M33RotDisk = M33.CircularVelocity(2,rs)
+    a = 30
+    M33RotHalo = M33.CircularVelocity(1,rs) #Calculates the circular velocity for the enclosed halo mass
+    M33RotDisk = M33.CircularVelocity(2,rs) #Calculates the circular velocity for the enclosed disk mass
     #M33 does not have a Bulge
-    M33RotCurve=M33.CircularVelocityTotal(rs)
-    M33HernquistCirc = M33.HernquistVCirc(rs,a,M31massHalo)
+    M33RotCurve=M33.CircularVelocityTotal(rs) #Calculates the circular velocity for the enclosed total mass
+    M33HernquistCirc = M33.HernquistVCirc(rs,a,M31massHalo) #Calculates the circular velocity for the hernquist mass
     ax = plt.subplot(111)
     plt.plot(rs,M33RotHalo, label='Halo Rotation Curve')
     plt.plot(rs,M33RotDisk, label='Disk Rotation Curve')
@@ -201,6 +258,7 @@ if __name__ == '__main__' :
     plt.legend()
     plt.show()
     plt.savefig('M33Vel.png')
-    ##I could have shortened the plotting part of the code but it was not that much extra##
-    #####Fix Hernquist Profile for both########?????????????????????
-    #####Comment######
+    '''
+    ##I could have shortened the plotting part of the code with a for loop
+    but it was not that much extra to just copy paste it twice##
+    '''
