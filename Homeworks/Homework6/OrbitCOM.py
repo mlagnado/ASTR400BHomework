ca0a019e40ba44c74f8cc06f92ad38ddad78df81
mlagnado <mlagnado@arizona.edu>
2025-02-24 16:51:49 -0700

    Created vector difference function

--- Homeworks/Homework6/OrbitCOM.py
+++ Homeworks/Homework6/OrbitCOM.py
@@ -53,7 +53,6 @@ def Orbit_COM(galaxy, start, end, n):
     '''
     # initialize the array for orbital info: t, x, y, z, vx, vy, vz of COM
     orbit = np.zeros([len(snap_ids),7])
-    print(orbit.shape)
     # a for loop 
     for i, snap_id in enumerate(snap_ids): # loop over files
         print(f"Currently computing snap number {snap_id}, counter: {i}")
@@ -93,37 +92,65 @@ def Orbit_COM(galaxy, start, end, n):
 # Recover the orbits and generate the COM files for each galaxy
 # read in 800 snapshots in intervals of n=5
 # Note: This might take a little while - test your code with a smaller number of snapshots first! 
-Orbit_COM("MW",0,800,5)
-
-
+#Orbit_COM("MW",0,801,5) #Only do if want to redo calculation ~3min
+#Orbit_COM("M31",0,801,5)
+#Orbit_COM("M33",0,801,5)
 
 # Read in the data files for the orbits of each galaxy that you just created
 # headers:  t, x, y, z, vx, vy, vz
 # using np.genfromtxt
-
-
-
+MW = np.genfromtxt('Orbit_MW.txt')
+print('MW table', MW.shape)
+print(MW)
+M31 = np.genfromtxt('Orbit_M31.txt')
+print('M31 table', M31.shape)
+print(M31)
+M33 = np.genfromtxt('Orbit_M33.txt')
+print('M33 table', M33.shape)
+print(M33)
 
 # function to compute the magnitude of the difference between two vectors 
 # You can use this function to return both the relative position and relative velocity for two 
 # galaxies over the entire orbit  
-
-
+def vector_dif(vector1, vector2):
+    '''
+    This functin computes the difference of two vectors
+    Inputs:
+        vector1 (array) array of position and velocity coords output from Orbit_COM()
+        vector2 (array of same shape as vector 1) vector to be subtracted from vector1 also output from Orbit_COM()
+        PV (string) tells wether we want the position of velocity
+    Outputs:
+        magnitude (float) the magnitude of the difference of the two vectors
+    '''
+    magnitude = np.zeros([len(vector1),2])
+    for i in range(len(vector1)):
+        x = vector1[i][1]-vector2[i][1] #difference in x pos
+        y = vector1[i][2]-vector2[i][2] #difference in y pos
+        z = vector1[i][3]-vector2[i][3] #difference in z pos
+        vx = vector1[i][4]-vector2[i][4] #difference in x vel
+        vy = vector1[i][5]-vector2[i][5] #difference in y vel
+        vz = vector1[i][6]-vector2[i][6] #difference in z vel
+        magnitude[i][0] = np.sqrt(x**2+y**2+z**2)
+        magnitude[i][1] = np.sqrt(vx**2+vy**2+vz**2)
+    return magnitude
 
 
 # Determine the magnitude of the relative position and velocities 
 
 # of MW and M31
-
+#print(vector_dif(MW,M31))
 # of M33 and M31
-
+#print(vector_dif(M33,M31))
 
 
 
 # Plot the Orbit of the galaxies 
 #################################
-
-
+fig = plt.figure()
+plt.plot(MW[:,0], vector_dif(MW,M31)[:,0])
+plt.xlabel('Time [Gyr]')
+plt.ylabel('Seperatino [kpc]')
+plt.title('Seperation between MW and M31 over time')
 
 
 # Plot the orbital velocities of the galaxies 
