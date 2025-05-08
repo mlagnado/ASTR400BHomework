Fixed lists, calculation issues, and added plot

Research_Assignment/ResearchAssignment7/DispersionSteps.py
@@ -36,11 +36,11 @@ Time_list =  Find_Timesteps('MW','M31')
 #for i in Time_list:
 #    print(Jacobi_Rad(i, 'M31', 'MW'))
 
-def Time_at_snap(list, galaxy1, galaxy2):
+def Time_at_snap(snaps, galaxy1, galaxy2):
     '''
-    This function takes in a list of snap numbers and returns the Time and separation of two galaxies at those snap numbers
+    This function takes in an array of snap numbers and returns the Time and separation of two galaxies at those snap numbers
     input:
-        list: array of integers, Includes all the snap numbers
+        snaps: array of integers, Includes all the snap numbers
         galaxy1: string, the galaxy that the dispersion will be calculated for
         galaxy2: string, the other galaxy that impacts jacobi radius
     output:
@@ -51,11 +51,11 @@ def Time_at_snap(list, galaxy1, galaxy2):
     Galaxy2 = np.genfromtxt(f'Orbit_{galaxy2}.txt')
     Separation = vector_dif(Galaxy1, Galaxy2) #Outputs the difference in position and velocity
     All_Time = Galaxy1[:,0] #Is the same for galaxy 1 or 2
-    Time = np.zeros(len(list))
-    Sep = np.zeros(len(list))
-    for i in range(len(list)): #get the times and separations for these time steps
-        Time[i] = All_Time[list[i]]
-        Sep[i] = Separation[list[i],0]
+    Time = np.zeros(len(snaps))
+    Sep = np.zeros(len(snaps))
+    for i in range(len(snaps)): #get the times and separations for these time steps
+        Time[i] = All_Time[snaps[i]]
+        Sep[i] = Separation[snaps[i],0]
 #    print(Sep, Time)
     return Sep, Time
 
@@ -71,7 +71,7 @@ plt.xlabel('Time [Gyr]')
 plt.ylabel('Seperation [kpc]')
 plt.title('Seperation between MW and M31 over time')
 plt.legend()
-plt.savefig('MW-M31sep.png')
+plt.savefig('MLagnado_Separation.png')
 plt.show()
 
 
@@ -92,24 +92,41 @@ plt.show()
 #plt.savefig(f'MW_VelocityDispersion1.png')
 #plt.show()
 
-fig = plt.figure()
+fig,ax = plt.subplots(nrows=1,ncols=2,figsize=(10,6))
 cmap = plt.get_cmap('cool') 
 color_val = np.linspace(0,1,len(Time_list))
 for i in range(len(Time_list)):
     disp, disp2, rad = calculate_dispersion(Time_list[i], 'MW','M31')
     color = cmap(color_val[i])
-    plt.plot(rad[1:],disp2[1:], linewidth=2, alpha=0.5, label=f'Dispersion of MW at t={Times[i]}Gyr', color=color)
+    ax[0].plot(rad[1:],disp[1:], linewidth=2, alpha=0.5, label=f'Dispersion of MW at t={np.round(Times[i],1)}Gyr', color=color)
 
-plt.ylabel('Dispersion [km/s]')
-plt.xlabel('Radius [kpc]')
-plt.title(f'Dispersion vs Radius')
-plt.legend()
-plt.savefig(f'MW_VelocityDispersion.png')
-plt.show()
+ax[0].set_ylabel('Dispersion [km/s]')
+ax[0].set_xlabel('Radius [kpc]')
+ax[0].set_title(f'Dispersion vs Radius')
+ax[0].legend()
+
 
 #print quantified values
+#disp0, disp02, rad = calculate_dispersion(Time_list[0], 'MW', 'M31')
+#dispL, dispL2, rad = calculate_dispersion(Time_list[1], 'MW', 'M31')
+#difference = dispL2-disp02
+#print(difference[-4:])
+#print(rad[-4:])
+
+
 disp0, disp02, rad = calculate_dispersion(Time_list[0], 'MW', 'M31')
-dispL, dispL2, rad = calculate_dispersion(Time_list[-1], 'MW', 'M31')
-difference = dispL2-disp02
-print(difference[-4:])
+for i in range(len(Time_list)):
+    if i == 0:
+        continue
+    disp, disp2, rad = calculate_dispersion(Time_list[i], 'MW','M31')
+    difference = disp2-disp0
+    print(difference[-4:])
+    color = cmap(color_val[i])
+    ax[1].plot(rad[1:],difference[1:], linewidth=2, alpha=0.5, label=f'Dispersion Change of MW at t={np.round(Times[i],1)}Gyr', color=color)
 print(rad[-4:])
+ax[1].set_ylabel('Dispersion [km/s]')
+ax[1].set_xlabel('Radius [kpc]')
+ax[1].set_title(f'Dispersion Change vs Radius')
+ax[1].legend()
+plt.savefig(f'MLagnado_Dispersion.png')
+plt.show()

Research_Assignment/ResearchAssignment7/GalaxyMass.py
@@ -1,8 +1,9 @@
 import numpy as np
 from ReadFile import Read
 import astropy.units as u
+from CenterOfMass import CenterOfMass
 
-def ComponentMass(filename, particle_type,distance):
+def ComponentMass(filename, particle_type, distance):
 	'''
 	This function sums the total mass of one specified particle type
 	inputs: filename (string) where the data will be read from
@@ -11,11 +12,16 @@ def ComponentMass(filename, particle_type,distance):
 	output: total_mass (astropy quantity) the total mass of a particular particle type in the data
 	'''
 	time, total, data = Read(filename) #reads the data
+	COM = CenterOfMass(filename, 2)
+	COM_pos = COM.COM_P(0.1,2.0) #Obtain COM coordinates
+	data['x'] = data['x'] - COM_pos[0].value #Shifts coordinates to be centered on COM
+	data['y'] = data['y'] - COM_pos[1].value
+	data['z'] = data['z'] - COM_pos[2].value
 	r = np.sqrt(data['x']**2 + data['y']**2 + data['z']**2)
-	index = np.where(r <= distance)
+	index = np.where(r <= distance) #Only looking for mass within the distance from the galaxy
 	data = data[index]
 	mass = np.sum(data[data['type'] == particle_type]['m'])
-	return mass*1e10/1e12
+	return mass*1e10
 
 #Code used to get each value in table
 #Changed file between MW, M31, and M33 and component from 1,2,and 3

Research_Assignment/ResearchAssignment7/VelocityDispersion.py
@@ -58,8 +58,8 @@ def Find_Timesteps(G1, G2):
 
     # Combine maxima and minima indices
     snap_indices = np.concatenate([maxima_indices, minima_indices])
-    
-    snap_nums = np.array([0] + list(snap_indices))
+    snap_nums = np.insert(snap_indices,-1,0) #Lastly we can't forget snapshot 0 as a baseline.
+#    snap_nums = np.array([0] + list(snap_indices))
     snap_nums = np.sort(snap_nums)
 
     return snap_nums
@@ -102,12 +102,12 @@ def Jacobi_Rad(snap, galaxy1, galaxy2):
     G1_filename = galaxy1 + '/' + galaxy1 + '_' + snap_num + '.txt' #opens the correct file
 
     R_0 = Separation(snap, galaxy1, galaxy2) #Utilizes previous fxn for separation
-
+#    print(R_0)
     G1_Halo = ComponentMass(G1_filename, 1, R_0) #calculates mass of component of the mass enclosed in the separation of the two galaxies
     G1_Disk = ComponentMass(G1_filename, 2, R_0)
     G1_Bulge = ComponentMass(G1_filename, 3, R_0)
     G1_mass = G1_Bulge + G1_Disk + G1_Halo #adds mass of all components
-
+#    print(G1_mass)
     #same is done for the other galaxy
     G2_filename = galaxy2 + '/' + galaxy2 + '_' + snap_num + '.txt'
     G2_Halo = ComponentMass(G2_filename, 1, R_0)
@@ -219,47 +219,53 @@ def calculate_dispersion(snap,galaxy1, galaxy2,r=2.0/1):
         current_vz = current_vz[index_high]
         current_vth = current_vth[index_high]
         current_circ = current_circ[index_high]
+        '''
+        This is all commented out because np.std() completes the same job
+        '''
 #        print(len(current_vel))
         #If our bounds didn't catch any particles this makes sure we don't have an error
         #It is mostly used far beyond the galaxy >30kpc for some rare cases
-        if len(current_vel) < 3: 
-            i += 1
-            continue
-        #Now find avg velocity in this shell
-        mean_V = np.sum(current_vel)/len(current_vel) #avg vel
-        mean_Vx = np.sum(current_vx)/len(current_vx) #x avg vel
-        mean_Vy = np.sum(current_vy)/len(current_vy)
-        mean_Vz = np.sum(current_vz)/len(current_vz)
-        mean_vth = np.sum(current_vth)/len(current_vth)
-        mean_circ = np.sum(current_circ)/len(current_circ)
+#        if len(current_vel) < 3: 
+#            i += 1
+#            continue
+#        #Now find avg velocity in this shell
+#        mean_V = np.sum(current_vel)/len(current_vel) #avg vel
+#        mean_Vx = np.sum(current_vx)/len(current_vx) #x avg vel
+#        mean_Vy = np.sum(current_vy)/len(current_vy)
+#        mean_Vz = np.sum(current_vz)/len(current_vz)
+#        mean_vth = np.sum(current_vth)/len(current_vth)
+#        mean_circ = np.sum(current_circ)/len(current_circ)
         
         #And calculate dispersion in this shell from the mean
-        sqr_dif = 0
-        sqr_difx = 0
-        sqr_dify = 0
-        sqr_difz = 0
-        sqr_difth = 0
-        sqr_circ = 0
+#        sqr_dif = 0
+#        sqr_difx = 0
+#        sqr_dify = 0
+#        sqr_difz = 0
+#        sqr_difth = 0
+#        sqr_circ = 0
 
         #calculates the square difference for the values in our bounds
-        for k in range(len(current_vel)):
-            sqr_dif += (current_vel[k] - mean_V)**2
-            sqr_difx += (current_vx[k] - mean_Vx)**2
-            sqr_dify += (current_vy[k] - mean_Vy)**2
-            sqr_difz += (current_vz[k] - mean_Vz)**2
-            sqr_difth += (current_vth[k] - mean_vth)**2
-            sqr_circ += (current_circ[k] - mean_circ)**2
+#        for k in range(len(current_vel)):
+#            sqr_dif += (current_vel[k] - mean_V)**2
+#            sqr_difx += (current_vx[k] - mean_Vx)**2
+#            sqr_dify += (current_vy[k] - mean_Vy)**2
+#            sqr_difz += (current_vz[k] - mean_Vz)**2
+#            sqr_difth += (current_vth[k] - mean_vth)**2
+#            sqr_circ += (current_circ[k] - mean_circ)**2
 #            print(sqr_dif)
         #Lastly we calculate the dispersion for each of our velocities
-        dispersion = np.sqrt((1/len(current_vel)) * sqr_dif)
-        dispersionx = np.sqrt((1/len(current_vx)) * sqr_difx)
-        dispersiony = np.sqrt((1/len(current_vy)) * sqr_dify)
-        dispersionz = np.sqrt((1/len(current_vz)) * sqr_difz)
-        dispersionth = np.sqrt((1/len(current_vth)) * sqr_difth)
-        dispersioncirc = np.sqrt((1/len(current_circ)) * sqr_circ)
+#        dispersion = np.sqrt((1/len(current_vel)) * sqr_dif)
+#        dispersionx = np.sqrt((1/len(current_vx)) * sqr_difx)
+#        dispersiony = np.sqrt((1/len(current_vy)) * sqr_dify)
+#        dispersionz = np.sqrt((1/len(current_vz)) * sqr_difz)
+#        dispersionth = np.sqrt((1/len(current_vth)) * sqr_difth)
+#        dispersioncirc = np.sqrt((1/len(current_circ)) * sqr_circ)
 
         #Now for our second method we find the magnitude of this dispersion
-        dispersion2 = np.sqrt(dispersionx**2 + dispersiony**2 + dispersionz**2)
+#        dispersion2 = np.sqrt(dispersionx**2 + dispersiony**2 + dispersionz**2)
+        #try out np.std()
+        dispersion = np.std(current_vel) #Calculates the dispersion at this timestep
+        dispersion2 = np.std(np.sqrt(current_vx**2 + current_vy**2 + current_vz**2)) #method two gets the same results
 
         #Now add all of these values to the list
         dispersion_list[i] = dispersion
@@ -277,9 +283,11 @@ Testing it out
 Time_list =  Find_Timesteps('MW','M31')
 #Galaxy1 = np.genfromtxt(f'Orbit_MW.txt')
 #for i in Time_list:
+#    print(i)
 #    print(Galaxy1[i][0])
 #print(Time_list)
 for i in Time_list:
+    print(i)
     print(Jacobi_Rad(i, 'M31', 'MW'))
 
 #On each of those snap numbers produce a plot that shows the dispersion vs the radius